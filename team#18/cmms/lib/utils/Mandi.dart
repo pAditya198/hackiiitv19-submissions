6cb8b34ae559e18141df5497398d257338ca0f15
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-10-05 13:54:02 +0530

    Added dependencies | common properties

---
+++ team#18/cmms/lib/utils/Mandi.dart
@@ -0,0 +1,16 @@
+class Mandi {
+  //common strings
+  static String appName = "Mandi";
+
+  //pages titles
+  static String profile = "Your profile";
+  static String homePage = "Homepage";
+  static String signIn = "Sign In";
+  static String register = "Register Yourself";
+
+  //common messages
+  static String error = "Some Error occured";
+
+  //prefs
+  static String languagePref = "hindi";
+}
